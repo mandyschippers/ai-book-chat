69a192043207aefc5ea1541f49852f316b4af745
mandyschippers <mandyschippers@hotmail.com>
2023-03-14 13:05:57 +1100

    constants js dev prod

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "https://ai-book-chat.herokuapps.com";
+    : "https://ai-book-chat.herokuapp.com";
