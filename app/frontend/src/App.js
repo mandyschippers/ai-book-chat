1e64f117ac7350bfa0cbf26c38b75ea60c304264
mandyschippers <mandyschippers@hotmail.com>
2023-03-14 12:30:23 +1100

    rebuild js

---
+++ app/frontend/src/App.js
@@ -0,0 +1,102 @@
+import axios from "axios";
+import { format } from "date-fns";
+import { useEffect, useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Book from "./components/Book";
+import { BASE_URL } from "./constants";
+
+//create axios headers to allow Access-Control-Allow-Origin
+axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+
+function App() {
+  const [book, setBook] = useState("");
+  const [characters, setCharacters] = useState("");
+  const [handle, setHandle] = useState("");
+
+  const handleBookChange = (e) => {
+    setBook(e.target.value);
+  };
+
+  const handleCharacterChange = (e) => {
+    setCharacters(e.target.value);
+  };
+
+  const handleHandleChange = (e) => {
+    setHandle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let data = {
+      book: book,
+      characters: characters,
+    };
+    axios.post(BASE_URL + "/api/books", data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = async () => {
+    const response = await axios.get(`${BASE_URL}/api/books`);
+    console.log("data", response.data);
+  };
+
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books/charlottes-web">Charlotte's Web</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/">
+          <div className="App">
+            <header className="App-header">
+              <form onSubmit={handleSubmit}>
+                <label htmlFor="book">Book</label>
+                <input
+                  type="text"
+                  name="book"
+                  id="book"
+                  value={book}
+                  onChange={handleBookChange}
+                />
+                <label htmlFor="characters">Characters</label>
+                <input
+                  type="text"
+                  name="characters"
+                  id="characters"
+                  value={characters}
+                  onChange={handleCharacterChange}
+                />
+                <label htmlFor="handle">Handle</label>
+                <input
+                  type="text"
+                  name="handle"
+                  id="handle"
+                  value={handle}
+                  onChange={handleHandleChange}
+                />
+                <button type="submit">Submit</button>
+              </form>
+            </header>
+          </div>
+        </Route>
+        <Route
+          path="/books/:handle"
+          render={({ match }) => <Book handle={match.params.handle} />}
+        />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
