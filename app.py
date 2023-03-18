91ae2cd5d5d68f8f1847117ed024a8e50fcfa4cf
mandyschippers <mandyschippers@hotmail.com>
2023-03-18 17:57:09 +1100

    updated styles

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ model_id = os.environ.get('MODEL')
 
 def get_initial_message(character, book):
     return [
-        {"role": "system", "content": "Answer the following question as if you are " + character + " from " + book + ". Analyse the writing style of " + character + " and answer as they would. If the answer cannot be found in the book " +
+        {"role": "system", "content": "Answer the following question as if you are " + character + " from the book " + book + ". Analyse the writing style of " + character + " and answer as they would. If the answer cannot be found in the book " +
             book + ", say you don\'t know the answer to that question. If the question is inappropriate for a 10 year old, say that you are not going to dignify that question with an answer. Answer as " + character + "."}
     ]
 
